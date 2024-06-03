3d1fb46fabc6dbfc760828727a1ce83fe07c5afd
Alexandra Ciancimino <noreply>
2024-06-02 23:32:55 -0700

    updates testimonials

--- testimonials.php
+++ testimonials.php
@@ -1,12 +1,13 @@
 <?php include('includes/header.php'); ?>
 
 <div class="main-wrapper">
+    <h1>Testimonials</h1>
 
-    <div class="main-row">
-
-    </div>
-    <!-- end main-row -->
+    <div class="portfolio-text">
+        <p>need to add!</p>
+    </div> 
 
 </div>
+<!-- end main-wrappers -->
 
 <?php include('includes/footer.php'); ?>
