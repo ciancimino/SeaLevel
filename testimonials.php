74a025ace71a24c55e1ffe609d03591190f7630e
Alexandra Ciancimino <noreply>
2024-06-04 14:03:22 -0700

    updates

--- testimonials.php
+++ testimonials.php
@@ -3,9 +3,22 @@
 <div class="main-wrapper">
     <h1>Testimonials</h1>
 
-    <div class="portfolio-text">
-        <p>need to add!</p>
+    <div class="main-row">
+
+    <div class="main-textbox">
+    <blockquote>"Bill from Sea Level Builders did an outstanding job on our renovation. He added beautiful floating shelves that transformed our living space and designed custom cabinets specifically for our vinyl collection. His attention to detail and craftsmanship exceeded our expectations. We couldn't be happier with the results!"</blockquote>
+    <p>Homeowner, NAME</p>
     </div> 
+    <!-- end main-textbox -->
+
+    <div class="main-textbox">
+    <blockquote>"Jesse from Sea Level Builders went above and beyond during our project. He wasn't even hired to do it, but he took the time to remove a stubborn tree stump from our front yard. His willingness to help and dedication to customer satisfaction truly set him apart. We're incredibly grateful for his hard work and kindness!"</blockquote>
+    <p>Homeowner, NAME</p>
+    </div> 
+    <!-- end main-textbox -->
+
+    </div>
+    <!-- end main-row -->
 
 </div>
 <!-- end main-wrappers -->
